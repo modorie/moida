040333681980d05f5cb7d862c6c337319c8a12bd
seokzin <seokzin95@gmail.com>
2022-02-20 20:46:41 +0900

    feat: HTML 헤더 정보 추가 정의 및 정렬
    - title 속성 외에는 전부 `_document.tsx` 파일에서 관리

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,6 +2,7 @@ import Document, {
   DocumentContext,
   DocumentInitialProps,
   Html,
+  Head,
   Main,
   NextScript,
 } from "next/document";
@@ -39,6 +40,17 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
+        <Head>
+          <meta charSet="utf-8" />
+          <link rel="icon" href="/favicon.svg" />
+          <meta property="og:title" content="" />
+          <meta property="og:image" content="" />
+          <meta property="og:description" content="" />
+          <meta property="og:url" content="//" />
+          <meta name="description" content="" />
+          <meta name="keywords" content="" />
+        </Head>
+
         <body>
           <Main />
           <NextScript />
