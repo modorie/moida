da6f4b6989c36f59d05f004f1fa0e589a4c0bf5e
devpla <chaeyeonhee@kakao.com>
2022-02-22 16:04:11 +0900

    feat: 스토리북 기본 테마 설정

---
+++ .storybook/theme.ts
@@ -0,0 +1,9 @@
+import { create } from "@storybook/theming";
+// import LogoStodybook from "../public/images/logo-storybook.png";
+
+export default create({
+  base: "light",
+  brandTitle: "Moida Design System",
+  brandUrl: "https://moida-ui.vercel.app/",
+  brandImage: "/images/logo-storybook.svg",
+});
