c5144a25a52eca18e41c148bb9556c0944463b3e
devpla <chaeyeonhee@kakao.com>
2022-02-26 23:43:30 +0900

    feat: 기본 FormProps 정의

---
+++ types/FormProps.ts
@@ -0,0 +1,10 @@
+import type { DisableProps } from "./ComponentProps";
+
+interface FormOptions {
+  hasError?: boolean;
+  required?: boolean;
+  readonly?: boolean;
+  disabled?: boolean;
+}
+
+export interface FormProps extends DisableProps, FormOptions {}
