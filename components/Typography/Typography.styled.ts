ecc58d788c0bd57c6cc8d47d8e5da601c64aa432
devpla <chaeyeonhee@kakao.com>
2022-02-26 23:48:35 +0900

    feat: TypoStyle 기본 `font-size` 추가

--- components/Typography/Typography.styled.ts
+++ components/Typography/Typography.styled.ts
@@ -5,7 +5,7 @@ import type TypoProps from "./Typography.types";
 
 export const TypoStyle = css<TypoProps>`
   font-family: "Spoqa Han Sans Neo", sans-serif;
-  font-size: ${({ size }) => size && FontSize[size]}px;
+  font-size: ${({ size }) => (size ? FontSize[size] : FontSize.body2)}px;
   font-weight: ${({ weight }) => weight && FontWeight[weight]};
   color: ${({ color }) => color && Theme.textColor[color]};
   line-height: 130%;
