9d09cec87df707ffeb8c960a8f0129d313e69224
seokzin <seokzin95@gmail.com>
2022-02-28 23:41:34 +0900

    feat: 스토리북에 Global Style 적용

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,3 +1,14 @@
+import { addDecorator } from "@storybook/react";
+// TODO : Alias 코드 동작은 하나 빨간 줄 출력
+import GlobalStyle from "@/styles/global";
+
+addDecorator((story) => (
+  <>
+    <GlobalStyle />
+    {story()}
+  </>
+));
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
