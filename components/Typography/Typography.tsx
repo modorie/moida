0c3fd50e075b8a5b97e29d3b507853db68209599
devpla <chaeyeonhee@kakao.com>
2022-03-01 22:40:02 +0900

    fix: 불필요한 default props를 제거하여 타입 오류 수정

--- components/Typography/Typography.tsx
+++ components/Typography/Typography.tsx
@@ -1,16 +1,10 @@
 import React from "react";
 
 import TypoView from "./Typography.styled";
-import { FontSize, FontWeight, Theme } from "@/foundations";
 
 import type TypoProps from "./Typography.types";
 
-const Typography = ({
-  weight = FontWeight.regular,
-  size = FontSize.body2,
-  color = Theme.textColor.black,
-  children,
-}: TypoProps) => {
+const Typography = ({ weight, size, color, children }: TypoProps) => {
   return (
     <TypoView weight={weight} size={size} color={color}>
       {children}
