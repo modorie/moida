9680c86ea75e9711319317637a0f8838aad833f0
seokzin <seokzin95@gmail.com>
2022-03-02 22:49:37 +0900

    feat: Story Accessibility addon 설치

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,7 +6,11 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "@storybook/addon-a11y",
   ],
+  typescript: {
+    check: true,
+  },
   framework: "@storybook/react",
   staticDirs: ["../public"],
   core: {
