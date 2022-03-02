6aa215bd8744b70e9bdebea18caf976d17429a3f
devpla <chaeyeonhee@kakao.com>
2022-03-02 21:34:31 +0900

    refac: TypoProps export 및 Input 컴포넌트 import문 간소화

--- components/Typography/index.ts
+++ components/Typography/index.ts
@@ -1,4 +1,6 @@
 import { TypoStyle } from "./Typography.styled";
+import type TypoProps from "./Typography.types";
 
 export { default as Typography } from "./Typography";
 export { TypoStyle };
+export type { TypoProps };
