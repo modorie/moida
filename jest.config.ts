2f166ef7d83b075746dff6789a7cf57550af0bce
seokzin <seokzin95@gmail.com>
2022-03-02 10:22:02 +0900

    chore: Jest 설정에 alias 추가

--- jest.config.ts
+++ jest.config.ts
@@ -8,6 +8,9 @@ const customJestConfig = {
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   moduleDirectories: ["node_modules", "<rootDir>/"],
   testEnvironment: "jest-environment-jsdom",
+  moduleNameMapper: {
+    "@/(.*)$": "<rootDir>/$1",
+  },
 };
 
 module.exports = createJestConfig(customJestConfig);
