1471d342dab1f9d89a763ac45821c6e67b20054e
devpla <chaeyeonhee@kakao.com>
2022-03-02 21:25:38 +0900

    feat: 수도권 지하철 노선 LineColor 정의

---
+++ components/StationInfo/LineColor/LineColor.types.ts
@@ -0,0 +1,5 @@
+import type { LineName } from "../StationInfo.types";
+
+type LineColorType = Record<LineName, string>;
+
+export default LineColorType;
---
+++ components/StationInfo/LineColor/index.ts
@@ -0,0 +1,1 @@
+export { default as LineColor } from "./LineColor";
