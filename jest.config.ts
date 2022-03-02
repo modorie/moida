ec8502ff9000258e156b5dec2bca99180bcc2523
seokzin <seokzin95@gmail.com>
2022-03-02 10:20:22 +0900

    feat: Jest 초기 설정

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+import nextJest from "next/jest";
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const customJestConfig = {
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  moduleDirectories: ["node_modules", "<rootDir>/"],
+  testEnvironment: "jest-environment-jsdom",
+};
+
+module.exports = createJestConfig(customJestConfig);
