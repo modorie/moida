1153abf6ce71c1e2ce87ae42fc09a42f50c60338
devpla <chaeyeonhee@kakao.com>
2022-03-04 22:23:44 +0900

    refac: Icon assets return문 제거

--- components/Icon/assets/Edit.tsx
+++ components/Icon/assets/Edit.tsx
@@ -1,31 +1,29 @@
 import React from "react";
 
-const Edit = (props: React.SVGProps<SVGSVGElement>) => {
-  return (
-    <svg
-      viewBox="0 0 30 30"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      {...props}
-    >
-      <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M21.2035 13.4418L11.1453 23.5H6.503L6.5 18.8547L16.5582 8.79651L21.2035 13.4418Z"
-        stroke="currentColor"
-        strokeWidth="1.8"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <path
-        d="M16.5582 8.79655L18.2903 7.06447C19.0429 6.31184 20.2632 6.31184 21.0158 7.06447L22.9355 8.98421C23.6882 9.73684 23.6882 10.9571 22.9355 11.7097L21.2034 13.4418V13.4418"
-        stroke="currentColor"
-        strokeWidth="1.8"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-    </svg>
-  );
-};
+const Edit = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    viewBox="0 0 30 30"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M21.2035 13.4418L11.1453 23.5H6.503L6.5 18.8547L16.5582 8.79651L21.2035 13.4418Z"
+      stroke="currentColor"
+      strokeWidth="1.8"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M16.5582 8.79655L18.2903 7.06447C19.0429 6.31184 20.2632 6.31184 21.0158 7.06447L22.9355 8.98421C23.6882 9.73684 23.6882 10.9571 22.9355 11.7097L21.2034 13.4418V13.4418"
+      stroke="currentColor"
+      strokeWidth="1.8"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
 
 export default Edit;
--- components/Icon/assets/X.tsx
+++ components/Icon/assets/X.tsx
@@ -1,29 +1,27 @@
 import React from "react";
 
-const X = (props: React.SVGProps<SVGSVGElement>) => {
-  return (
-    <svg
-      viewBox="0 0 30 30"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      {...props}
-    >
-      <path
-        d="M8 8L22 22"
-        stroke="currentColor"
-        strokeWidth="1.8"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <path
-        d="M8 22L22 8"
-        stroke="currentColor"
-        strokeWidth="1.8"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-    </svg>
-  );
-};
+const X = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    viewBox="0 0 30 30"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M8 8L22 22"
+      stroke="currentColor"
+      strokeWidth="1.8"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M8 22L22 8"
+      stroke="currentColor"
+      strokeWidth="1.8"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
 
 export default X;
