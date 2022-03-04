16fff77e7ab1158b4b1431428ed1ed22583e6615
devpla <chaeyeonhee@kakao.com>
2022-03-04 22:55:27 +0900

    refac: StationInfo 컴포넌트 return문 제거

--- components/StationInfo/StationInfo.tsx
+++ components/StationInfo/StationInfo.tsx
@@ -7,19 +7,17 @@ import stations from "./assets/stations";
 
 import type StationType from "./StationInfo.types";
 
-const StationInfo = ({ name }: StationType) => {
-  return (
-    <Layout>
-      <SymbolBox>
-        {stations[name]?.map((lineName) => (
-          <LineSymbol name={lineName} key={lineName} />
-        ))}
-      </SymbolBox>
-      <NameBox color={stations[name]?.[0]}>
-        <Typography>{name.slice(0, -1)}</Typography>
-      </NameBox>
-    </Layout>
-  );
-};
+const StationInfo = ({ name }: StationType) => (
+  <Layout>
+    <SymbolBox>
+      {stations[name]?.map((lineName) => (
+        <LineSymbol name={lineName} key={lineName} />
+      ))}
+    </SymbolBox>
+    <NameBox color={stations[name]?.[0]}>
+      <Typography>{name.slice(0, -1)}</Typography>
+    </NameBox>
+  </Layout>
+);
 
 export default StationInfo;
