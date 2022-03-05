437814b6624021c5a832d7131274254a874390e3
devpla <chaeyeonhee@kakao.com>
2022-03-06 00:54:12 +0900

    feat: 스토리북 기본 테마 변경

--- .storybook/theme.ts
+++ .storybook/theme.ts
@@ -3,6 +3,8 @@ import { create } from "@storybook/theming";
 export default create({
   base: "light",
   brandTitle: "Moida Design System",
-  brandUrl: "https://moida-ui.vercel.app/",
   brandImage: "/images/logo-storybook.svg",
+
+  colorPrimary: "#34B6EA",
+  colorSecondary: "#34B6EA",
 });
