e66e792016f1e3d349818a50090f57a2f6c2287f
devpla <chaeyeonhee@kakao.com>
2022-03-06 20:30:44 +0900

    feat: 누락된 index.ts 파일 추가

--- components/index.ts
+++ components/index.ts
@@ -1,4 +1,9 @@
+export { default as Badge } from "./Badge";
+export { default as GpsButton } from "./GpsButton";
+export { default as HelperText } from "./HelperText";
 export { default as Icon } from "./Icon";
 export { default as Input } from "./Input";
+export { default as Logo } from "./Logo";
+export { default as MapMark } from "./MapMark";
 export { default as StationInfo } from "./StationInfo";
 export { default as Typography } from "./Typography";
