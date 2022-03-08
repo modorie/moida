33042864484f76273e87c2564fab3989b8f6fd92
devpla <chaeyeonhee@kakao.com>
2022-03-08 23:33:20 +0900

    docs: Tab 컴포넌트 스토리 페이지 추가

--- components/Tabs/Tab/Tab.stories.tsx
+++ components/Tabs/Tab/Tab.stories.tsx
@@ -1,16 +1,26 @@
 import React from "react";
+import { Story } from "@storybook/react";
 
 import Tab from "./Tab";
+
+import type { TabProps } from "./Tab.types";
 
 export default {
   title: "Components/Tabs/Tab",
   component: Tab,
 };
 
-export const Default = ({ ...args }) => <Tab {...args} />;
+export const Default: Story<TabProps> = ({ ...args }) => <Tab {...args} />;
 
 Default.args = {
   active: true,
   children: "텍스트",
   onClick: () => {},
 };
+
+export const Examples = () => (
+  <>
+    <Tab active>텍스트</Tab>
+    <Tab>텍스트</Tab>
+  </>
+);
