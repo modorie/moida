bf1d07c68ad6f6c6c97d6ba6818238a5d311c308
devpla <chaeyeonhee@kakao.com>
2022-03-08 23:54:04 +0900

    refac: 불필요한 코드 제거

--- components/Tabs/Tabs.tsx
+++ components/Tabs/Tabs.tsx
@@ -1,27 +1,29 @@
-import React, { useState, useCallback, useMemo } from "react";
+import React, { useState } from "react";
 import { Tab } from "./Tab";
 
 import { Layout } from "./Tabs.styled";
+
 import type { TabsProps } from "./Tabs.types";
 
 const Tabs = ({ tabItems }: TabsProps) => {
   const [currentTabIndex, setCurrentTabIndex] = useState<number>(0);
-  const handleClickTab = useCallback((tabIndex: number) => {
+
+  const handleClickTab = (tabIndex: number) => {
     setCurrentTabIndex(tabIndex);
-  }, []);
+  };
 
-  const Content = useMemo(() => {
-    return tabItems.map((item, index) => (
-      <Tab
-        key={index}
-        active={index === currentTabIndex}
-        children={item}
-        onClick={() => handleClickTab(index)}
-      />
-    ));
-  }, [currentTabIndex, handleClickTab]);
-
-  return <Layout>{Content}</Layout>;
+  return (
+    <Layout>
+      {tabItems.map((item, index) => (
+        <Tab
+          key={index}
+          active={index === currentTabIndex}
+          children={item}
+          onClick={() => handleClickTab(index)}
+        />
+      ))}
+    </Layout>
+  );
 };
 
 export default Tabs;
