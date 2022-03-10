7e7edcd176685415658a90f47facfdef9f213de4
devpla <chaeyeonhee@kakao.com>
2022-03-10 23:11:26 +0900

    feat: NavBody 컴포넌트 초안 생성

---
+++ components/Navbar/NavBody/NavBody.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { Layout } from "./NavBody.styled";
+import { NavItem } from "@/components/Navbar";
+
+const NavBody = () => {
+  return (
+    <Layout>
+      <NavItem>모임 관리</NavItem>
+    </Layout>
+  );
+};
+
+export default NavBody;
