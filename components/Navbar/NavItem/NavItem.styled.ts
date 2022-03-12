597322794da0381f8a9de54270cd49eab3f115d8
devpla <chaeyeonhee@kakao.com>
2022-03-12 11:48:52 +0900

    feat: NavItem hover 및 active 인터렉션 추가

--- components/Navbar/NavItem/NavItem.styled.ts
+++ components/Navbar/NavItem/NavItem.styled.ts
@@ -1,13 +1,29 @@
 import styled from "styled-components";
 
+import { Theme, Transition } from "@/foundations";
+
 export const Layout = styled.li`
   display: flex;
   justify-content: space-between;
   align-items: center;
 
   height: 50px;
+  margin: 0 -15px;
+  padding: 0 15px;
+  border-radius: 4px;
+
   list-style: none;
+  transition: ${Transition};
+  user-select: none;
   cursor: pointer;
+
+  :hover {
+    background-color: ${Theme.bgColor.lighter};
+  }
+
+  :active {
+    background-color: ${Theme.bgColor.light};
+  }
 
   svg {
     position: relative;
