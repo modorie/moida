8048fd6c51ed99065d8fb7eb5932d4986c1b4a8e
seokzin <seokzin95@gmail.com>
2022-03-13 22:00:57 +0900

    feat: 버튼 색상 팔레트 타입 선언

--- foundations/Color/Theme/Theme.types.ts
+++ foundations/Color/Theme/Theme.types.ts
@@ -45,6 +45,51 @@ export type helperBorderColor =
   | "light"
   | "dark";
 
+// prettier-ignore
+export type ButtonBgColor = 
+|  "primary" 
+|  "secondary"
+
+// prettier-ignore
+export type ButtonTextColor = 
+|  "primary" 
+|  "secondary"
+
+// prettier-ignore
+export type ButtonBorderColor = 
+|  "primary" 
+|  "secondary"
+
+// prettier-ignore
+export type ButtonActiveBgColor = 
+|  "primary" 
+|  "secondary"
+
+// prettier-ignore
+export type ButtonActiveTextColor = 
+|  "primary" 
+|  "secondary"
+
+// prettier-ignore
+export type ButtonActiveBorderColor = 
+|  "primary" 
+|  "secondary"
+
+// prettier-ignore
+export type ButtonDisableBgColor = 
+|  "primary" 
+|  "secondary"
+
+// prettier-ignore
+export type ButtonDisableTextColor = 
+|  "primary" 
+|  "secondary"
+
+// prettier-ignore
+export type ButtonDisableBorderColor = 
+|  "primary" 
+|  "secondary"
+
 interface ThemeType {
   [category: string]: { [color: string]: string };
   bgColor: Record<bgColor, string>;
@@ -53,7 +98,15 @@ interface ThemeType {
   borderColor: Record<borderColor, string>;
   textColor: Record<textColor, string>;
   helperBgColor: Record<helperBgColor, string>;
-  helperBorderColor: Record<helperBorderColor, string>;
+  ButtonBgColor: Record<ButtonBgColor, string>;
+  ButtonTextColor: Record<ButtonTextColor, string>;
+  ButtonBorderColor: Record<ButtonBorderColor, string>;
+  ButtonActiveBgColor: Record<ButtonActiveBgColor, string>;
+  ButtonActiveTextColor: Record<ButtonActiveTextColor, string>;
+  ButtonActiveBorderColor: Record<ButtonActiveBorderColor, string>;
+  ButtonDisableBgColor: Record<ButtonDisableBgColor, string>;
+  ButtonDisableTextColor: Record<ButtonDisableTextColor, string>;
+  ButtonDisableBorderColor: Record<ButtonDisableBorderColor, string>;
 }
 
 export default ThemeType;
