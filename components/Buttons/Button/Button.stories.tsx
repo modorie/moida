535adc872bad6c40d5ddf1ef05027235920fdecd
seokzin <seokzin95@gmail.com>
2022-03-14 00:48:16 +0900

    feat: Button 스토리북 레이아웃 변경

--- components/Buttons/Button/Button.stories.tsx
+++ components/Buttons/Button/Button.stories.tsx
@@ -6,6 +6,9 @@ import type { ButtonProps } from "./Button.types";
 export default {
   title: "Components/Buttons/Button",
   component: Button,
+  parameters: {
+    layout: "centered",
+  },
 } as Meta;
 
 const Template: Story<ButtonProps> = (args) => <Button {...args} />;
