17a54ff69d48176ef6e0044a54649181d71d1d7d
devpla <chaeyeonhee@kakao.com>
2022-03-15 20:57:37 +0900

    feat: Avatar 컴포넌트 isHost prop 및 HostBadge 추가

--- components/Avatar/Avatar.tsx
+++ components/Avatar/Avatar.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 
 import { Layout, Emoji } from "./Avatar.styled";
+import { HostBadge } from "./";
 
 import AvatarProps, { EmojiObj } from "./Avatar.types";
 
-const Avatar = ({ emoji }: AvatarProps) => {
+const Avatar = ({ emoji, isHost }: AvatarProps) => {
   return (
     <Layout color={EmojiObj[emoji]}>
       <Emoji size="sub1">{emoji}</Emoji>
+      {isHost && <HostBadge />}
     </Layout>
   );
 };
