81a01a311e264e699568cf19de461632d0a1920e
devpla <chaeyeonhee@kakao.com>
2022-03-15 20:56:54 +0900

    design: Avatar position 속성 추가

--- components/Avatar/Avatar.styled.ts
+++ components/Avatar/Avatar.styled.ts
@@ -13,6 +13,7 @@ export const Layout = styled.div<LayoutProps>`
   display: inline-flex;
   justify-content: center;
   align-items: center;
+  position: relative;
 
   width: 45px;
   height: 45px;
