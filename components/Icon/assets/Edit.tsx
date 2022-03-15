d26538c73aaa0c2a8c161b64523e1662e980a4b4
seokzin <seokzin95@gmail.com>
2022-03-16 01:48:37 +0900

    fix: 케밥 케이스를 카멜 케이스로 변경 및 일부 값 복원
    - 피그마 exported SVG를 덮어 씌우면서 바뀐 strokee, d 값도 복구했어요.

--- components/Icon/assets/Edit.tsx
+++ components/Icon/assets/Edit.tsx
@@ -8,20 +8,20 @@ const Edit = (props: React.SVGProps<SVGSVGElement>) => (
     {...props}
   >
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M15.2438 8.30854L7.55225 16.0001H4.00229L4 12.4479L11.6916 4.75628L15.2438 8.30854Z"
-      stroke="#333333"
-      stroke-width="1.8"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      stroke="currentColor"
+      strokeWidth="1.8"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
     <path
       d="M11.6914 4.75619L13.0159 3.43166C13.5915 2.85611 14.5246 2.85611 15.1002 3.43166L16.5682 4.89969C17.1437 5.47523 17.1437 6.40837 16.5682 6.98391L15.2437 8.30844V8.30844"
-      stroke="#333333"
-      stroke-width="1.8"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      stroke="currentColor"
+      strokeWidth="1.8"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
   </svg>
 );
