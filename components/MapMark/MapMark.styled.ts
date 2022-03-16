510beb7820870d8a99e72347651c98614f64659d
devpla <chaeyeonhee@kakao.com>
2022-03-16 21:40:22 +0900

    feat: MapMark Children prop 및 Text 영역 추가

--- components/MapMark/MapMark.styled.ts
+++ components/MapMark/MapMark.styled.ts
@@ -1,9 +1,45 @@
 import styled from "styled-components";
-import { Theme } from "@/foundations";
+
+import { TypoStyle } from "@/components/Typography";
+import { Theme, Palette } from "@/foundations";
 
 export const Layout = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+
+  height: 80px;
+`;
+
+export const Text = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   position: relative;
-  transform: translate(13px, 13px);
+
+  height: 35px;
+  padding: 0 16px;
+  border-radius: 8px;
+
+  /* TODO : Theme.bgColor에 추가 */
+  background-color: ${Palette.blue300};
+  ${TypoStyle}
+
+  ::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    z-index: -1;
+
+    height: 65px;
+    width: 0px;
+    border-left: 2px solid ${Palette.blue300};
+  }
+`;
+
+export const Mark = styled.div`
+  position: relative;
 
   width: 19px;
   height: 19px;
