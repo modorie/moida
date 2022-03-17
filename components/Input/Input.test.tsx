98b3f59c3f5d9ed77ad22f364e0b1ef0c13cb6c1
seokzin <seokzin95@gmail.com>
2022-03-17 17:17:43 +0900

    feat: 기존 테스트 파일 및 스냅샷 삭제

--- components/Input/Input.test.tsx
+++
@@ -1,7 +0,0 @@
-import { render } from "@testing-library/react";
-import { Input } from "@/components";
-
-it("matches snapshot", () => {
-  const utils = render(<Input />);
-  expect(utils.container).toMatchSnapshot();
-});
--- components/Typography/Typography.test.tsx
+++
@@ -1,7 +0,0 @@
-import { render } from "@testing-library/react";
-import { Typography } from "@/components";
-
-it("matches snapshot", () => {
-  const utils = render(<Typography />);
-  expect(utils.container).toMatchSnapshot();
-});
