7770f588090bd978cba7ac832e8855974a1115f5
seokzin <seokzin95@gmail.com>
2022-03-18 23:43:28 +0900

    test: 비활성화 버튼 테스트 코드 작성

--- components/Buttons/Button/Button.test.tsx
+++ components/Buttons/Button/Button.test.tsx
@@ -2,12 +2,17 @@ import { render, screen } from "@testing-library/react";
 import Button from "./Button";
 import { Primary, Secondary } from "./Button.stories";
 
-test("Primary 버튼을 렌더링", () => {
+test("Primary 버튼 렌더링", () => {
   render(<Button {...Primary.args} />);
   expect(screen.getByRole("button")).toHaveTextContent("text");
 });
 
-test("Secondary 버튼을 렌더링", () => {
+test("Secondary 버튼 렌더링", () => {
+  render(<Button {...Secondary.args} />);
+  expect(screen.getByRole("button")).toHaveTextContent("text");
+});
+
+test("Disable 버튼 렌더링", () => {
   render(<Button {...Secondary.args} />);
   expect(screen.getByRole("button")).toHaveTextContent("text");
 });
