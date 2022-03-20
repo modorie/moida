3c05993e1ba5a5841ef7a4633d0fd9489959d18a
seokzin <seokzin95@gmail.com>
2022-03-20 21:16:15 +0900

    test: 버튼 스타일에 따른 describe 분할

--- components/Buttons/Button/Button.test.tsx
+++ components/Buttons/Button/Button.test.tsx
@@ -5,35 +5,39 @@ import Button from "./Button";
 import { Primary, Secondary } from "./Button.stories";
 
 describe("Button Test >", () => {
-  it("Primary Button >", () => {
-    const { getByRole } = render(<Button {...Primary.args} />);
-    const primaryButton = getByRole("button");
+  describe("Primary Button >", () => {
+    it("Default >", () => {
+      const { getByRole } = render(<Button {...Primary.args} />);
+      const primaryButton = getByRole("button");
 
-    expect(primaryButton).toHaveStyle(
-      `color: ${Theme.ButtonTextColor.primary}`
-    );
-    expect(primaryButton).toHaveStyle(
-      `background-color: ${Theme.ButtonBgColor.primary};`
-    );
-    expect(primaryButton).toHaveStyle(
-      `border: 1px solid ${Theme.borderColor.primary}`
-    );
-    expect(primaryButton).toHaveStyle(`font-weight: ${FontWeight.bold}`);
+      expect(primaryButton).toHaveStyle(
+        `color: ${Theme.ButtonTextColor.primary}`
+      );
+      expect(primaryButton).toHaveStyle(
+        `background-color: ${Theme.ButtonBgColor.primary};`
+      );
+      expect(primaryButton).toHaveStyle(
+        `border: 1px solid ${Theme.borderColor.primary}`
+      );
+      expect(primaryButton).toHaveStyle(`font-weight: ${FontWeight.bold}`);
+    });
   });
 
-  it("Secondary Button >", () => {
-    const { getByRole } = render(<Button {...Secondary.args} />);
-    const secondaryButton = getByRole("button");
+  describe("Secondary Button >", () => {
+    it("Default >", () => {
+      const { getByRole } = render(<Button {...Secondary.args} />);
+      const secondaryButton = getByRole("button");
 
-    expect(secondaryButton).toHaveStyle(
-      `color: ${Theme.ButtonTextColor.secondary}`
-    );
-    expect(secondaryButton).toHaveStyle(
-      `background-color: ${Theme.ButtonBgColor.secondary};`
-    );
-    expect(secondaryButton).toHaveStyle(
-      `border: 1px solid ${Theme.borderColor.primary}`
-    );
-    expect(secondaryButton).toHaveStyle(`font-weight: ${FontWeight.regular}`);
+      expect(secondaryButton).toHaveStyle(
+        `color: ${Theme.ButtonTextColor.secondary}`
+      );
+      expect(secondaryButton).toHaveStyle(
+        `background-color: ${Theme.ButtonBgColor.secondary};`
+      );
+      expect(secondaryButton).toHaveStyle(
+        `border: 1px solid ${Theme.borderColor.primary}`
+      );
+      expect(secondaryButton).toHaveStyle(`font-weight: ${FontWeight.regular}`);
+    });
   });
 });
