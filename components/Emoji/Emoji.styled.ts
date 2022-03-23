da15b6fbe4bec2606984bd2ef8e47c319b32a7f5
devpla <chaeyeonhee@kakao.com>
2022-03-23 22:20:13 +0900

    design: Emoji 위치 css 조정

--- components/Emoji/Emoji.styled.ts
+++ components/Emoji/Emoji.styled.ts
@@ -7,6 +7,10 @@ import type EmojiProps from "./Emoji.types";
 import type TypoProps from "@/components/Typography/Typography.types";
 
 export const EmojiText = styled.i<EmojiProps & TypoProps>`
+  display: inline-block;
+  position: relative;
+  top: -0.1em;
+
   ${TypoStyle}
   font-family: emoji;
   font-style: normal;
