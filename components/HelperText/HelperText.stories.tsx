b6b0caa48b059b6f3c6d982b2c4678c1546f9c69
seokzin <seokzin95@gmail.com>
2022-09-04 20:33:09 +0900

    fix: children 프로퍼티를 합성 컴포넌트로 대체
    - no-children-prop 규칙

--- components/HelperText/HelperText.stories.tsx
+++ components/HelperText/HelperText.stories.tsx
@@ -1,33 +1,33 @@
-import styled from "styled-components";
-import { Story, Meta } from "@storybook/react";
+import styled from 'styled-components'
+import { Story, Meta } from '@storybook/react'
 
-import HelperText from "./HelperText";
+import HelperText from './HelperText'
 
-import type HelperProps from "./HelperText.types";
+import type HelperProps from './HelperText.types'
 
 export default {
-  title: "Components/HelperText",
+  title: 'Components/HelperText',
   component: HelperText,
   parameters: {
-    layout: "centered",
+    layout: 'centered',
   },
-} as Meta;
+} as Meta
 
-export const Default: Story<HelperProps> = (args) => <HelperText {...args} />;
+export const Default: Story<HelperProps> = (args) => <HelperText {...args} />
 
 Default.args = {
-  children: "텍스트",
-  color: "light",
-};
+  children: '텍스트',
+  color: 'light',
+}
 
 export const Examples = () => (
   <Layout>
-    <HelperText children="하람" color="light" />
-    <HelperText children="중간장소" color="dark" />
+    <HelperText color="light">하람</HelperText>
+    <HelperText color="dark">중간장소</HelperText>
   </Layout>
-);
+)
 
 const Layout = styled.div`
   display: flex;
   gap: 10px;
-`;
+`
