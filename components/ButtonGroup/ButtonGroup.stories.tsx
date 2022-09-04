68b028ca7e85d9a3652eb6a0bd3c6c606708a5a3
seokzin <seokzin95@gmail.com>
2022-09-04 20:43:36 +0900

    fix: 타입 변경에 따른 입력값 변경

--- components/ButtonGroup/ButtonGroup.stories.tsx
+++ components/ButtonGroup/ButtonGroup.stories.tsx
@@ -1,32 +1,32 @@
-import { Story, Meta } from "@storybook/react";
+import { Story, Meta } from '@storybook/react'
 
-import ButtonGroup from "./ButtonGroup";
+import ButtonGroup from './ButtonGroup'
 
-import type { ButtonGroupProps } from "./ButtonGroup.types";
+import type { ButtonGroupProps } from './ButtonGroup.types'
 
 export default {
-  title: "Components/ButtonGroup",
+  title: 'Components/ButtonGroup',
   component: ButtonGroup,
-} as Meta;
+} as Meta
 
 export const Default: Story<ButtonGroupProps> = (args) => (
   <ButtonGroup {...args} />
-);
+)
 
 Default.args = {
-  label: "중간 장소 추천",
-  name: "group",
+  label: '중간 장소 추천',
+  name: 'group',
   options: [
     {
-      id: "option1",
-      label: "필요해요",
-      value: true,
+      id: 'option1',
+      label: '필요해요',
+      value: 'true',
       defaultChecked: true,
     },
     {
-      id: "option2",
-      label: "다음에 받을래요",
-      value: false,
+      id: 'option2',
+      label: '다음에 받을래요',
+      value: 'false',
     },
   ],
-};
+}
