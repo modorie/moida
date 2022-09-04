a3258a5312ec3474d4d1faccbc0bd1582e9a9cf9
seokzin <seokzin95@gmail.com>
2022-09-04 21:49:58 +0900

    refac: 타입 단언을 통해 any 타입 제거
    - includes는 집합 관계를 엄격히 따져요.
      (https://github.com/Microsoft/TypeScript/issues/26255)
    
    - unknown을 통한 이중 타입 단언으로 타입을 조심스래 맞춰야 해요.
      (이펙티브 타입스크립트 item 9)
    
    - 그리고 size 또한 undefined가 아님을 보장해야 돼요.

--- components/Text/Text.styled.ts
+++ components/Text/Text.styled.ts
@@ -1,26 +1,28 @@
-import styled, { css } from "styled-components";
+import styled, { css } from 'styled-components'
 
-import { TitleList } from "@/foundations/FontSize";
+import { FontSizeKey, TitleList } from '@/foundations/FontSize'
 
-import type { TextProps } from "./Text.types";
+import type { TextProps } from './Text.types'
 
 export const TextStyle = css<TextProps>`
-  font-size: ${({ size = "body1", theme }) => theme.fontSize[size]}px;
-  font-weight: ${({ weight = "regular", theme }) =>
-    weight === "inherit" ? weight : theme.fontWeight[weight]};
-  color: ${({ color = "black", theme }) =>
-    color === "currentColor" ? color : theme.color[color]};
+  font-size: ${({ size = 'body1', theme }) => theme.fontSize[size]}px;
+  font-weight: ${({ weight = 'regular', theme }) =>
+    weight === 'inherit' ? weight : theme.fontWeight[weight]};
+  color: ${({ color = 'black', theme }) =>
+    color === 'currentColor' ? color : theme.color[color]};
   line-height: 130%;
-  letter-spacing: ${({ size }: any) =>
-    TitleList.includes(size) ? "-0.03rem" : "-0.02rem"};
+  letter-spacing: ${({ size }) =>
+    size && (TitleList as unknown as FontSizeKey).includes(size)
+      ? '-0.03rem'
+      : '-0.02rem'};
   text-align: ${({ align }) => align};
   white-space: ${({ whiteSpace }) => whiteSpace};
-  font-style: ${({ italic }) => (italic ? "italic" : "normal")};
-  text-decoration: ${({ underline }) => (underline ? "underline" : "none")};
+  font-style: ${({ italic }) => (italic ? 'italic' : 'normal')};
+  text-decoration: ${({ underline }) => (underline ? 'underline' : 'none')};
   user-select: ${({ userSelect }) => userSelect};
   text-overflow: ${({ textOverflow }) => textOverflow};
-`;
+`
 
 export const TextView = styled.span<TextProps>`
   ${TextStyle}
-`;
+`
--- components/Typography/Typography.styled.ts
+++ components/Typography/Typography.styled.ts
@@ -1,20 +1,22 @@
-import styled, { css } from "styled-components";
-import { FontSize, FontWeight, Theme } from "@/foundations";
+import styled, { css } from 'styled-components'
+import { FontSize, FontWeight, Theme } from '@/foundations'
 
-import { TitleList } from "@/foundations/FontSize";
-import type TypoProps from "./Typography.types";
+import { FontSizeKey, TitleList } from '@/foundations/FontSize'
+import type TypoProps from './Typography.types'
 
 export const TypoStyle = css<TypoProps>`
   font-size: ${({ size }) => (size ? FontSize[size] : FontSize.body2)}px;
   font-weight: ${({ weight }) => weight && FontWeight[weight]};
   color: ${({ color }) => color && Theme.textColor[color]};
   line-height: 130%;
-  letter-spacing: ${({ size }: any) =>
-    TitleList.includes(size) ? "-0.03rem" : "-0.02rem"};
-`;
+  letter-spacing: ${({ size }) =>
+    size && (TitleList as unknown as FontSizeKey).includes(size)
+      ? '-0.03rem'
+      : '-0.02rem'};
+`
 
 const Typography = styled.span<TypoProps>`
   ${TypoStyle}
-`;
+`
 
-export default Typography;
+export default Typography
