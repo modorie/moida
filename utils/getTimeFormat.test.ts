005a973c2a76d48c324b326aee4f24e9a88319bb
seokzin <seokzin95@gmail.com>
2022-09-20 00:19:09 +0900

    test: 테스트 요소 간소화
    - 테스트에서 new Date()를 2번 호출하는 동안 1ms 오차 발생
    - 연/월/일만 테스트 하도록 간소화
    - 아주 낮은 확률로 사이드이펙트 존재 ('일'이 바뀔 때)

--- utils/getTimeFormat.test.ts
+++ utils/getTimeFormat.test.ts
@@ -3,17 +3,11 @@ import { getTimeFormat } from './getTimeFormat'
 describe('getTimeFormat(date) >', () => {
   it('return current time format by default >', () => {
     const now = new Date()
-    const { year, month, date, day, hour, min, sec, ms, totalMs } =
-      getTimeFormat()
+    const { year, month, date, day } = getTimeFormat()
 
     expect(year).toBe(now.getFullYear())
     expect(month).toBe(now.getMonth())
     expect(date).toBe(now.getDate())
     expect(day).toBe(now.getDay())
-    expect(hour).toBe(now.getHours())
-    expect(min).toBe(now.getMinutes())
-    expect(sec).toBe(now.getSeconds())
-    expect(ms).toBe(now.getMilliseconds())
-    expect(totalMs).toBe(now.getTime())
   })
 })
