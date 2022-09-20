8e421d11eaa3680f3b16cb59d04d57dc57b86da5
seokzin <seokzin95@gmail.com>
2022-09-20 23:46:38 +0900

    test: getMonth의 시작 값이 0

--- utils/getTimeFormat.test.ts
+++ utils/getTimeFormat.test.ts
@@ -16,4 +16,15 @@ describe('getTimeFormat(date) >', () => {
     expect(ms).toBe(now.getMilliseconds())
     expect(totalMs).toBe(now.getTime())
   })
+
+  it('return month between 0 and 11 >', () => {
+    expect(getTimeFormat(new Date('2022-01-01')).month).toBe(0)
+    expect(getTimeFormat(new Date('2022-12-01')).month).toBe(11)
+
+    expect(getTimeFormat(new Date('January 1, 2022')).month).toBe(0)
+    expect(getTimeFormat(new Date('December 1, 2022')).month).toBe(11)
+
+    expect(getTimeFormat(new Date(2022, 1, 1)).month).toBe(1)
+    expect(getTimeFormat(new Date(2022, 12, 1)).month).toBe(0)
+  })
 })
