bc62155f2ac3eb621a9dd114efa022dad96e7cfa
seokzin <seokzin95@gmail.com>
2022-09-23 08:20:55 +0900

    test: 윤년 달력의 처음과 마지막날 체크

--- utils/generateCalendar.test.ts
+++ utils/generateCalendar.test.ts
@@ -2,7 +2,7 @@ import { generateCalendar } from './generateCalendar'
 import { getTimeFormat } from './getTimeFormat'
 
 describe('generateCalendar(dateObj) >', () => {
-  it('return today calendar >', () => {
+  it("return today's calendar >", () => {
     const now = new Date()
     const calendar = generateCalendar(now)
     const { year, month, date } = getTimeFormat(now)
@@ -19,19 +19,35 @@ describe('generateCalendar(dateObj) >', () => {
     ])
   })
 
-  it('return leap month calendar >', () => {
+  it("return leap month's calendar >", () => {
     const leapMonth = new Date('2020-02-29')
     const calendar = generateCalendar(leapMonth)
 
-    expect(calendar.filter((obj) => obj.today === true)).toEqual([
-      {
-        year: 2020,
-        month: 1,
-        date: 29,
-        today: true,
-        prev: false,
-        next: false,
-      },
-    ])
+    expect(calendar.filter((obj) => obj.today === true).at(0)).toEqual({
+      year: 2020,
+      month: 1,
+      date: 29,
+      today: true,
+      prev: false,
+      next: false,
+    })
+
+    expect(calendar.at(0)).toEqual({
+      year: 2020,
+      month: 0,
+      date: 26,
+      today: false,
+      prev: true,
+      next: false,
+    })
+
+    expect(calendar.at(-1)).toEqual({
+      year: 2020,
+      month: 1,
+      date: 29,
+      today: true,
+      prev: false,
+      next: false,
+    })
   })
 })
