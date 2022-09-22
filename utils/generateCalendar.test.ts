346a27577aa0e8b2824a1a74dd7dd2b95bf3f631
seokzin <seokzin95@gmail.com>
2022-09-22 14:27:52 +0900

    test: 윤년 정상 출력 테스트

--- utils/generateCalendar.test.ts
+++ utils/generateCalendar.test.ts
@@ -2,16 +2,32 @@ import { generateCalendar } from './generateCalendar'
 import { getTimeFormat } from './getTimeFormat'
 
 describe('generateCalendar(dateObj) >', () => {
-  const now = new Date()
-  const calendar = generateCalendar(now)
-  const { year, month, date } = getTimeFormat(now)
+  it('return today calendar >', () => {
+    const now = new Date()
+    const calendar = generateCalendar(now)
+    const { year, month, date } = getTimeFormat(now)
 
-  it('return today object in calendar >', () => {
     expect(calendar.filter((obj) => obj.today === true)).toEqual([
       {
         year,
         month,
         date,
+        today: true,
+        prev: false,
+        next: false,
+      },
+    ])
+  })
+
+  it('return leap month calendar >', () => {
+    const leapMonth = new Date('2020-02-29')
+    const calendar = generateCalendar(leapMonth)
+
+    expect(calendar.filter((obj) => obj.today === true)).toEqual([
+      {
+        year: 2020,
+        month: 1,
+        date: 29,
         today: true,
         prev: false,
         next: false,
