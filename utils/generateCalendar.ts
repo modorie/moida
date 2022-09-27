2e3a69986c1405a6446ee5d60b5b04ade0224c3d
seokzin <seokzin95@gmail.com>
2022-09-27 22:44:31 +0900

    fix: 변수값 바르게 변경

--- utils/generateCalendar.ts
+++ utils/generateCalendar.ts
@@ -19,71 +19,42 @@ interface CalendarItem {
  */
 
 export const generateCalendar = (dateObj: Date): CalendarItem[] => {
-  const { year, month } = getTimeFormat(dateObj)
+  const { year, month, date } = getTimeFormat(dateObj)
   const prevMonthLastDay = getTimeFormat(new Date(year, month, 0))
   const currentMonthFirstDay = getTimeFormat(new Date(year, month, 1))
   const currentMonthLastDay = getTimeFormat(new Date(year, month + 1, 0))
   const nextMonthFirstDay = getTimeFormat(new Date(year, month + 1, 1))
 
-  const prevMonthCalendar = range(0, currentMonthFirstDay.day, -1).map((i) => ({
-    year: prevMonthLastDay.year,
-    month: prevMonthLastDay.month,
-    date: prevMonthLastDay.date - i + 1,
-    today: false,
-    prev: true,
-    next: false,
-  }))
+  const prevMonthCalendar = range(currentMonthFirstDay.day, -1, -1).map(
+    (d) => ({
+      year: prevMonthLastDay.year,
+      month: prevMonthLastDay.month,
+      date: prevMonthLastDay.date - d + 1,
+      today: false,
+      prev: true,
+      next: false,
+    })
+  )
 
-  const currentMonthCalendar = range(1, currentMonthLastDay.date).map((i) => ({
-    year: prevMonthLastDay.year,
-    month: prevMonthLastDay.month,
-    date: prevMonthLastDay.date - i + 1,
-    today: false,
-    prev: true,
-    next: false,
-  }))
+  const currentMonthCalendar = range(1, currentMonthLastDay.date + 1).map(
+    (d) => ({
+      year,
+      month,
+      date: d,
+      today: d === date,
+      prev: false,
+      next: false,
+    })
+  )
 
-  const nextMonthCalendar = range(1, 6 - currentMonthLastDay.day).map((i) => ({
+  const nextMonthCalendar = range(1, 7 - currentMonthLastDay.day).map((d) => ({
     year: nextMonthFirstDay.year,
     month: nextMonthFirstDay.month,
-    date: i,
+    date: d,
     today: false,
     prev: false,
     next: true,
   }))
-
-  // for (let i = currentMonthFirstDay.day; i > 0; i--) {
-  //   calendar.push({
-  //     year: prevMonthLastDay.year,
-  //     month: prevMonthLastDay.month,
-  //     date: prevMonthLastDay.date - i + 1,
-  //     today: false,
-  //     prev: true,
-  //     next: false,
-  //   })
-  // }
-
-  // for (let i = 1; i <= currentMonthLastDay.date; i++) {
-  //   calendar.push({
-  //     year,
-  //     month,
-  //     date: i,
-  //     today: i === date,
-  //     prev: false,
-  //     next: false,
-  //   })
-  // }
-
-  // for (let i = 1; i <= 6 - currentMonthLastDay.day; i++) {
-  //   calendar.push({
-  //     year: nextMonthFirstDay.year,
-  //     month: nextMonthFirstDay.month,
-  //     date: i,
-  //     today: false,
-  //     prev: false,
-  //     next: true,
-  //   })
-  // }
 
   return [...prevMonthCalendar, ...currentMonthCalendar, ...nextMonthCalendar]
 }
