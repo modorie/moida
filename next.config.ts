838ca0a3b5da49ef196cfcb870df270eff90516a
seokzin <seokzin95@gmail.com>
2022-09-30 13:58:37 +0900

    chore: env 프로퍼티 제거

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,12 @@
 import type { NextConfig } from 'next'
 
-const nextConfig = {
-  webpack: (config: NextConfig) => {
+const nextConfig: NextConfig = {
+  webpack: (config) => {
     config.module.rules.push({
       test: /\.(jpe?g|png|svg|gif|ico|eot|ttf|woff|woff2|mp4|pdf|webm|txt)$/,
       loader: 'file-loader',
       options: {
         publicPath: 'public',
-      },
-      env: {
-        PUBLIC_KAKAO_MAP_API_KEY: process.env.PUBLIC_KAKAO_MAP_API_KEY,
       },
     })
 
