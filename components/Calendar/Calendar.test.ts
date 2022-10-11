362e65573988d5e17c1da197dd2d89f01a86d4ba
seokzin <seokzin95@gmail.com>
2022-10-11 16:56:47 +0900

    feat: Calendar 컴포넌트 템플릿 추가

---
+++ components/Calendar/Calendar.test.ts
@@ -0,0 +1,7 @@
+import Calendar from './Calendar'
+
+describe('Calendar', () => {
+  it('should be defined', () => {
+    expect(Calendar).toBeDefined()
+  })
+})
---
+++ components/Calendar/Calendar.tsx
@@ -0,0 +1,8 @@
+import { Container } from './Calendar.styled'
+import { CalendarProps } from './Calendar.types'
+
+const Calendar = ({ id, className, style }: CalendarProps) => {
+  return <Container>Calendar</Container>
+}
+
+export default Calendar
