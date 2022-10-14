b1f4ab6eeea28f7f360819310c232ef0f3db8efd
devpla <chaeyeonhee@kakao.com>
2022-10-15 01:06:11 +0900

    refac: 불필요한 컴포넌트, 상수 제거

--- components/Typography/index.ts
+++
@@ -1,6 +0,0 @@
-import { TypoStyle } from './Typography.styled'
-import type TypoProps from './Typography.types'
-
-export { default } from './Typography'
-export { TypoStyle }
-export type { TypoProps }
