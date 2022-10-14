6482b3d9be9604ee1dd6a5b682c4b3c07cf0fe6f
seokzin <seokzin95@gmail.com>
2022-10-14 16:28:10 +0900

    feat: 캘린더 이전/다음 달 출력 로직 구현

--- components/Calendar/Calendar.styled.ts
+++ components/Calendar/Calendar.styled.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
 
-export const Container = styled.div`
-  display: flex;
+export const Layout = styled.div`
+  padding: 4px;
 `
--- components/Calendar/Calendar.tsx
+++ components/Calendar/Calendar.tsx
@@ -1,8 +1,49 @@
-import { Container } from './Calendar.styled'
+import { useState } from 'react'
+
+import { chunk, generateCalendar, getTimeFormat } from '@/utils'
+
+import { Layout } from './Calendar.styled'
 import { CalendarProps } from './Calendar.types'
 
 const Calendar = ({ id, className, style }: CalendarProps) => {
-  return <Container>Calendar</Container>
+  const now = getTimeFormat(new Date())
+  const [date, setDate] = useState({ year: now.year, month: now.month })
+  const calendar = generateCalendar(date.year, date.month)
+
+  return (
+    <Layout id={id} className={className} style={style}>
+      <button
+        onClick={() => setDate({ year: date.year, month: date.month - 1 })}
+      >
+        ←
+      </button>
+
+      <span>
+        {date.year}년 {date.month}월
+      </span>
+
+      <button
+        onClick={() => setDate({ year: date.year, month: date.month + 1 })}
+      >
+        →
+      </button>
+
+      <div>
+        {['일', '월', '화', '수', '목', '금', '토'].map((day) => (
+          <span key={day}> {day} </span>
+        ))}
+      </div>
+      <div>
+        {chunk(calendar, 7).map((week, index) => (
+          <div key={index}>
+            {week.map((day) => (
+              <button key={day.date}> {day.date} </button>
+            ))}
+          </div>
+        ))}
+      </div>
+    </Layout>
+  )
 }
 
 export default Calendar
