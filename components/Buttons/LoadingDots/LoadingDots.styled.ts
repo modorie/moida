c47095c3e21bfc823f249f12582db1724cd32bb9
devpla <chaeyeonhee@kakao.com>
2022-10-15 00:57:51 +0900

    feat: LoadingDots 스타일 수정

--- components/Buttons/LoadingDots/LoadingDots.styled.ts
+++ components/Buttons/LoadingDots/LoadingDots.styled.ts
@@ -1,49 +1,53 @@
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 
-export const Layout = styled.div`
-  position: relative;
-  width: 8px;
-  height: 8px;
-  border-radius: 50%;
-  animation: dotFlashing 1s infinite linear alternate;
-  animation-delay: 0.5s;
+import { LoadingDotsProps } from './LoadingDots'
 
-  &::before,
-  &::after {
-    content: '';
-    display: inline-block;
-    position: absolute;
-    top: 0;
-  }
-
-  &::before {
-    left: -14px;
-    width: 8px;
-    height: 8px;
-    border-radius: 50%;
-    animation: dot-flashing 1s infinite alternate;
-    animation-delay: 0s;
-  }
-
-  &::after {
-    left: 14px;
-    width: 8px;
-    height: 8px;
-    border-radius: 50%;
-    animation: dot-flashing 1s infinite alternate;
-    animation-delay: 1s;
-  }
-
-  @keyframes dot-flashing {
+const dotFlashing = keyframes`
     0% {
-      opacity: 0.8;
-      background-color: white;
-      color: white;
+      opacity: 0.15;
+      transform: scale(0.9);
     }
 
     50%,
     100% {
       opacity: 1;
+      transform: scale(1);
     }
+`
+export const Layout = styled.div<LoadingDotsProps>`
+  position: relative;
+  height: 8px;
+
+  > span,
+  &::before,
+  &::after {
+    content: '';
+    position: absolute;
+    top: 0;
+
+    width: 8px;
+    height: 8px;
+    border-radius: 50%;
+
+    background-color: ${({ color = 'currentColor', theme }) =>
+      color === 'currentColor' ? color : theme.color[color]};
+    animation: ${dotFlashing} 0.9s infinite linear alternate;
+    opacity: 0;
+    transform: scale(0.8);
+  }
+
+  &::before {
+    left: -14px;
+    animation-delay: 0s;
+  }
+
+  > span {
+    left: 0;
+    animation-delay: 0.3s;
+  }
+
+  &::after {
+    left: 14px;
+    animation-delay: 0.6s;
   }
 `
