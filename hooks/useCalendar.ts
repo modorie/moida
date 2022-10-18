61544ed36ed085ebab581aac90db37c3c0a0d0c1
seokzin <seokzin95@gmail.com>
2022-10-18 11:11:24 +0900

    feat: 선택된 캘린더 아이템 타입 변경
    - 기존 Date 내장 객체를 활용해도 구현 가능하지만 탐색이 비효율적
    - o(n^2) -> o(n) 이지만 max(n) = 31 이기에 뚜렷한 성능 차이는 없는
      소소한 리팩

--- hooks/useCalendar.ts
+++ hooks/useCalendar.ts
@@ -1,26 +1,23 @@
 import { useEffect, useState } from 'react'
 
-import { generateCalendar } from '@/utils'
+import { CalendarItem, generateCalendar } from '@/utils'
 
 const useCalendar = (date: Date) => {
   const [month, setMonth] = useState(date.getMonth())
   const [year, setYear] = useState(date.getFullYear())
-  const [selected, setSelected] = useState<Date[]>([])
+  const [selected, setSelected] = useState<CalendarItem[]>([])
   const [calendar, setCalendar] = useState(generateCalendar({ month, year }))
 
   useEffect(() => {
     setCalendar(generateCalendar({ month, year }))
   }, [month, year])
 
-  const handleSelected = (date: Date) => {
-    const isSelected = selected.find((d) => d.getTime() === date.getTime())
-
+  const handleSelected = (day: CalendarItem) =>
     setSelected((prev) =>
-      isSelected
-        ? prev.filter((d) => d.getTime() !== date.getTime())
-        : [...prev, date]
+      selected.find((item) => item.id === day.id)?.selected
+        ? prev.filter((d) => d.id !== day.id)
+        : [...prev, { ...day, selected: true }]
     )
-  }
 
   const nextMonth = () =>
     month === 11 ? (setMonth(0), setYear(year + 1)) : setMonth(month + 1)
