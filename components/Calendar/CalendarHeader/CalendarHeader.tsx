10413e3181888e45b55c9f21b1820716942d2b25
seokzin <seokzin95@gmail.com>
2022-10-31 10:28:09 +0900

    refac: 중복된 CSS 코드 제거

--- components/Calendar/CalendarHeader/CalendarHeader.tsx
+++ components/Calendar/CalendarHeader/CalendarHeader.tsx
@@ -1,6 +1,6 @@
 import { Icon, Typography } from '@/components'
 
-import { Header, HeaderButton } from './CalendarHeader.styled'
+import { Layout, MonthButton } from './CalendarHeader.styled'
 
 interface CalendarHeaderProps {
   month: number
@@ -16,19 +16,19 @@ const CalendarHeader = ({
   nextMonth,
 }: CalendarHeaderProps) => {
   return (
-    <Header>
-      <HeaderButton onClick={prevMonth} aria-label="Previous month">
+    <Layout>
+      <MonthButton onClick={prevMonth} aria-label="Previous month">
         <Icon name="chevron-left" size={30} color="black" />
-      </HeaderButton>
+      </MonthButton>
 
       <Typography size="sub1" weight="bold">
         {year}년 {month + 1}월
       </Typography>
 
-      <HeaderButton onClick={nextMonth} aria-label="Next month">
+      <MonthButton onClick={nextMonth} aria-label="Next month">
         <Icon name="chevron-right" size={30} color="black" />
-      </HeaderButton>
-    </Header>
+      </MonthButton>
+    </Layout>
   )
 }
 
